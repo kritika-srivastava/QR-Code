1596793d6466cd5bc64c4d3ad5d212fd1fd3f0be
Kritika Srivastava <kritika.s1408@gmail.com>
2020-04-26 20:21:59 +0530

    Update reader.py

--- QR Code Reader/reader.py
+++ QR Code Reader/reader.py
@@ -12,6 +12,9 @@ import webbrowser
 #to let python open the linked webpage
 
 d=decode(Image.open(r'C:\Users\Kritika\Desktop\VSC\python\QR-Code\images\profile_qr.png'))
+#replace the path in quotes with the path to your png image along with the name of png image.
+
+
 # Saving the decoded in url
 url=(d[0].data.decode())
 webbrowser.open(url)
@@ -20,3 +23,8 @@ webbrowser.open(url)
 
 #While displaying a simple message encoded in qrcode comment the line 17 and uncomment line 22:
 #print(url)
+
+
+
+
+
